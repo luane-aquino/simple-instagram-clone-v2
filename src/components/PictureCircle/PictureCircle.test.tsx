450ebb50580d49438da97b15864611365a1f700d
Luane Aquino <luane.aquino.br@gmail.com>
2022-12-05 11:56:22 +0900

    added snapshot test to PictureCircle

--- src/components/PictureCircle/PictureCircle.test.tsx
+++ src/components/PictureCircle/PictureCircle.test.tsx
@@ -2,8 +2,9 @@ import { render, screen } from '@testing-library/react';
 import PictureCircle from './index';
 
 describe('PictureCircle', () => {
-  test('should render the component', () => {
-    render(<PictureCircle imageUrl='' name='nass' seen={false} />)
+  test('should render the component correctly', () => {
+    const {asFragment} = render(<PictureCircle imageUrl='' name='nass' seen={false} />)
+    expect(asFragment()).toMatchSnapshot()
     const button = screen.getByRole('button', {name: 'Story by nass, not seen'})
     expect(button).toBeVisible()
     const img = screen.getByRole('img', {name: 'user profile'})
